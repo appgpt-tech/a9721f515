415962e3278b526060421dc9a38b39830b5e8848
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 01:44:48 +0200

    created on Fri Jan 26 2024 23:44:36 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/data.ts
@@ -0,0 +1,3 @@
+
+let data: any = {"defaultData":{"Albums":[{"albumName":"Revolver","genre":"Rock"},{"albumName":"The Dark Side of the Moon","genre":"Psychedelic"},{"albumName":"Thriller","genre":"Pop"}],"Tracks":[{"trackName":"Yesterday","duration":2,"album":"Revolver"},{"trackName":"Eclipse","duration":2,"album":"The Dark Side of the Moon"},{"trackName":"Beat It","duration":4,"album":"Thriller"}]}};
+export default data;
