cdf14b808cd9cf9417bcb10a18a752c1aeb22499
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 01:59:05 +0200

    edited on Fri Jan 26 2024 23:58:53 GMT+0000 (Coordinated Universal Time)

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -1,3 +1,3 @@
 
-let data: any = {"defaultData":{"Albums":[{"albumName":"Revolver","genre":"Rock"},{"albumName":"The Dark Side of the Moon","genre":"Psychedelic"},{"albumName":"Thriller","genre":"Pop"}],"Tracks":[{"trackName":"Yesterday","duration":2,"album":"Revolver"},{"trackName":"Eclipse","duration":2,"album":"The Dark Side of the Moon"},{"trackName":"Beat It","duration":4,"album":"Thriller"}]}};
+let data: any = {"defaultData":{"Albums":[{"albumName":"Album 1","genre":"Pop"},{"albumName":"Album 2","genre":"Rock"},{"albumName":"Album 3","genre":"Jazz"}],"Tracks":[{"trackName":"Track 1","duration":"3:45","album":"Album 1","lyrics":"Lyrics for Track 1"},{"trackName":"Track 2","duration":"4:30","album":"Album 2","lyrics":"Lyrics for Track 2"},{"trackName":"Track 3","duration":"5:15","album":"Album 3","lyrics":"Lyrics for Track 3"}]}};
 export default data;
