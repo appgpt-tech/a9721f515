733d0b6f40e0f437693e0af60b4efff8da932cd3
11ai <124818466+11ai@users.noreply.github.com>
2024-01-27 01:44:47 +0200

    created on Fri Jan 26 2024 23:44:36 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/dashboard.tsx
@@ -0,0 +1,7 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+    export const Dashboard = () => (
+      <Card>
+      <CardHeader title= "music inventory" />
+      <CardContent> An application to manage the inventory of a music store < /CardContent>
+      </Card>
+    );
