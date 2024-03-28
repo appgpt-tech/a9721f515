4787df7ac19203d6527da2f25ae57e93d1e170e3
11ai <124818466+11ai@users.noreply.github.com>
2024-03-28 20:16:18 +0200

    edited on Thu Mar 28 2024 18:15:58 GMT+0000 (Coordinated Universal Time)

--- frontend/src/dataProvider.tsx
+++ frontend/src/dataProvider.tsx
@@ -8,61 +8,9 @@ import postgrestRestProvider from "@promitheus/ra-data-postgrest";
 import qs from 'qs';
 
 const fieldConfig = {
-    projects: [
+    Users: [
   {
-    "field": "projectName",
-    "datatype": "String"
-  },
-  {
-    "field": "description",
-    "datatype": "String"
-  },
-  {
-    "field": "startDate",
-    "datatype": "DateTime"
-  },
-  {
-    "field": "endDate",
-    "datatype": "DateTime"
-  },
-  {
-    "field": "status",
-    "datatype": "String"
-  },
-  {
-    "field": "id",
-    "datatype": "Integer"
-  }
-],
-datasets: [
-  {
-    "field": "datasetName",
-    "datatype": "String"
-  },
-  {
-    "field": "description",
-    "datatype": "String"
-  },
-  {
-    "field": "creationDate",
-    "datatype": "DateTime"
-  },
-  {
-    "field": "lastModifiedDate",
-    "datatype": "DateTime"
-  },
-  {
-    "field": "id",
-    "datatype": "Integer"
-  }
-],
-teamMembers: [
-  {
-    "field": "memberID",
-    "datatype": "Integer"
-  },
-  {
-    "field": "name",
+    "field": "username",
     "datatype": "String"
   },
   {
@@ -70,11 +18,355 @@ teamMembers: [
     "datatype": "String"
   },
   {
+    "field": "password",
+    "datatype": "String"
+  },
+  {
     "field": "role",
     "datatype": "String"
   },
   {
-    "field": "joinDate",
+    "field": "id",
+    "datatype": "Integer"
+  }
+],
+Vendors: [
+  {
+    "field": "companyName",
+    "datatype": "String"
+  },
+  {
+    "field": "contactName",
+    "datatype": "String"
+  },
+  {
+    "field": "email",
+    "datatype": "String"
+  },
+  {
+    "field": "password",
+    "datatype": "String"
+  },
+  {
+    "field": "id",
+    "datatype": "Integer"
+  }
+],
+Customers: [
+  {
+    "field": "customerName",
+    "datatype": "String"
+  },
+  {
+    "field": "email",
+    "datatype": "String"
+  },
+  {
+    "field": "password",
+    "datatype": "String"
+  },
+  {
+    "field": "billingAddress",
+    "datatype": "String"
+  },
+  {
+    "field": "shippingAddress",
+    "datatype": "String"
+  },
+  {
+    "field": "country",
+    "datatype": "String"
+  },
+  {
+    "field": "phone",
+    "datatype": "String"
+  },
+  {
+    "field": "id",
+    "datatype": "Integer"
+  }
+],
+Inventory: [
+  {
+    "field": "product",
+    "datatype": "Integer"
+  },
+  {
+    "field": "quantity",
+    "datatype": "Integer"
+  },
+  {
+    "field": "lowStockThreshold",
+    "datatype": "Integer"
+  },
+  {
+    "field": "id",
+    "datatype": "Integer"
+  }
+],
+Products: [
+  {
+    "field": "productName",
+    "datatype": "String"
+  },
+  {
+    "field": "vendor",
+    "datatype": "Integer"
+  },
+  {
+    "field": "price",
+    "datatype": "Real"
+  },
+  {
+    "field": "weight",
+    "datatype": "Real"
+  },
+  {
+    "field": "description",
+    "datatype": "String"
+  },
+  {
+    "field": "thumbnail",
+    "datatype": "String"
+  },
+  {
+    "field": "image",
+    "datatype": "String"
+  },
+  {
+    "field": "category",
+    "datatype": "Integer"
+  },
+  {
+    "field": "creationDate",
+    "datatype": "DateTime"
+  },
+  {
+    "field": "stock",
+    "datatype": "Integer"
+  },
+  {
+    "field": "id",
+    "datatype": "Integer"
+  }
+],
+ProductCategories: [
+  {
+    "field": "category",
+    "datatype": "String"
+  },
+  {
+    "field": "id",
+    "datatype": "Integer"
+  }
+],
+Discounts: [
+  {
+    "field": "product",
+    "datatype": "Integer"
+  },
+  {
+    "field": "discountType",
+    "datatype": "String"
+  },
+  {
+    "field": "description",
+    "datatype": "String"
+  },
+  {
+    "field": "discountAmount",
+    "datatype": "Real"
+  },
+  {
+    "field": "discountPercent",
+    "datatype": "Real"
+  },
+  {
+    "field": "id",
+    "datatype": "Integer"
+  }
+],
+ShoppingCart: [
+  {
+    "field": "customer",
+    "datatype": "Integer"
+  },
+  {
+    "field": "product",
+    "datatype": "Integer"
+  },
+  {
+    "field": "priceAtPurchase",
+    "datatype": "Real"
+  },
+  {
+    "field": "quantity",
+    "datatype": "Integer"
+  },
+  {
+    "field": "id",
+    "datatype": "Integer"
+  }
+],
+Orders: [
+  {
+    "field": "orderNumber",
+    "datatype": "String"
+  },
+  {
+    "field": "customer",
+    "datatype": "Integer"
+  },
+  {
+    "field": "totalAmount",
+    "datatype": "Real"
+  },
+  {
+    "field": "vat",
+    "datatype": "Real"
+  },
+  {
+    "field": "totalAmountWithVat",
+    "datatype": "Real"
+  },
+  {
+    "field": "shippingCost",
+    "datatype": "Real"
+  },
+  {
+    "field": "shippingAddress",
+    "datatype": "String"
+  },
+  {
+    "field": "orderAddress",
+    "datatype": "String"
+  },
+  {
+    "field": "orderEmail",
+    "datatype": "String"
+  },
+  {
+    "field": "orderDate",
+    "datatype": "DateTime"
+  },
+  {
+    "field": "orderStatus",
+    "datatype": "String"
+  },
+  {
+    "field": "trackingNo",
+    "datatype": "String"
+  },
+  {
+    "field": "id",
+    "datatype": "Integer"
+  }
+],
+OrderDetails: [
+  {
+    "field": "orderNumber",
+    "datatype": "Integer"
+  },
+  {
+    "field": "line",
+    "datatype": "Integer"
+  },
+  {
+    "field": "product",
+    "datatype": "Integer"
+  },
+  {
+    "field": "price",
+    "datatype": "Real"
+  },
+  {
+    "field": "quantity",
+    "datatype": "Integer"
+  },
+  {
+    "field": "id",
+    "datatype": "Integer"
+  }
+],
+Payments: [
+  {
+    "field": "orderNumber",
+    "datatype": "Integer"
+  },
+  {
+    "field": "amount",
+    "datatype": "Real"
+  },
+  {
+    "field": "paymentMethod",
+    "datatype": "String"
+  },
+  {
+    "field": "paymentDate",
+    "datatype": "DateTime"
+  },
+  {
+    "field": "paymentStatus",
+    "datatype": "String"
+  },
+  {
+    "field": "id",
+    "datatype": "Integer"
+  }
+],
+Reviews: [
+  {
+    "field": "product",
+    "datatype": "Integer"
+  },
+  {
+    "field": "customer",
+    "datatype": "Integer"
+  },
+  {
+    "field": "vendor",
+    "datatype": "Integer"
+  },
+  {
+    "field": "rating",
+    "datatype": "Real"
+  },
+  {
+    "field": "reviewDetails",
+    "datatype": "String"
+  },
+  {
+    "field": "date",
+    "datatype": "DateTime"
+  },
+  {
+    "field": "id",
+    "datatype": "Integer"
+  }
+],
+SupportTickets: [
+  {
+    "field": "user",
+    "datatype": "Integer"
+  },
+  {
+    "field": "customer",
+    "datatype": "Integer"
+  },
+  {
+    "field": "description",
+    "datatype": "String"
+  },
+  {
+    "field": "status",
+    "datatype": "String"
+  },
+  {
+    "field": "creationDate",
+    "datatype": "DateTime"
+  },
+  {
+    "field": "resolutionDate",
     "datatype": "DateTime"
   },
   {
